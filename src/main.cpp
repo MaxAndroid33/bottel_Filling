9d075f44ad369054430a2069cf0401e037dad4a5
MaxAndroid33 <yuosifmax309@gmail.com>
2022-12-07 18:53:07 +0300

    fix the code and edit pins for lcd end stepper

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,45 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
+#include <Wire.h>
 
+
+// Initialise the LCD with the arduino. LiquidCrystal(rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd(A6, A7, 2, 3, 4, 5);
 
 //--------------------------------------------
 
 //RELAY PINS************
-#define air_pump = 12;
-#define motor_conv = 11;
-#define pump_filling = 10;
-#define motor_cap = 9;
+#define air_pump  12
+#define motor_conv  11
+#define pump_filling  10
+#define motor_cap  9
 
 //------------------------------------------
 
 //IR SENSOR PINS**************
-#define ir_start = 8;
-#define ir_fill = 6;
-#define ir_CAP = 7;
+#define ir_start  8
+#define ir_fill  6
+#define ir_CAP  7
 //-----------------------------------------
 
 //BUZZER PIN**********************
-#define buz = A2;
+#define buz  A2
 //------------------------------------------
 
 
 //STEPPER MOTOR PINS********************
-#define en_stepper 3
-const int StepX = A3;
-const int DirX = A4;
+#define en_stepper 13
+#define StepX A3
+#define DirX  A4
 int pulseWidthMicros = 1000;  // microseconds
 int millisBtwnSteps = 15000;
 
+void CheckCap();
+void CheckBottol();
+void InitialPositionSet();
 void CircleRotorStepper(int x);
-// Initialise the LCD with the arduino. LiquidCrystal(rs, enable, d4, d5, d6, d7)
-LiquidCrystal lcd(A5, A6, 4, 5, 6, 7);
+void FillPump(int status);
+void CapSetPump(int status);
 
 void setup() {
 
